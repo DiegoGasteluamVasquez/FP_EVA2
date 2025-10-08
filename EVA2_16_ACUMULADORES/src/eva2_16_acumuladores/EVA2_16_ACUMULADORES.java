5058d0172af616c89c1d80059d2b41b8be0e76d4
DiegoGasteluamVasquez <L25550361@chihuahua2.tecnm.mx>
2025-10-08 17:46:22 -0600

    Add files via upload

---
+++ EVA2_16_ACUMULADORES/src/eva2_16_acumuladores/EVA2_16_ACUMULADORES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_16_acumuladores;
+
+/**
+ *
+ * @author DiegogvTEC ll
+ */
+public class EVA2_16_ACUMULADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int acum = 10000;
+        System.out.println(acum);
+        acum = acum + 200;
+        System.out.println(acum);
+        acum = acum + 500;
+        System.out.println(acum);
+        acum = acum -1200;
+        System.out.println(acum);
+        acum+=100;//acum = acum +100
+        System.out.println(acum);
+    }
+    
+}
