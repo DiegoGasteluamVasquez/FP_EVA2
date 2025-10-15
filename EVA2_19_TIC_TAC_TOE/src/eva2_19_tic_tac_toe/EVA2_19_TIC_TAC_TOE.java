33f6207fc8af7e0a2aa77155680c9d7e9debe8e4
DiegoGasteluamVasquez <L25550361@chihuahua2.tecnm.mx>
2025-10-15 17:51:40 -0600

    Add files via upload

---
+++ EVA2_19_TIC_TAC_TOE/src/eva2_19_tic_tac_toe/EVA2_19_TIC_TAC_TOE.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_19_tic_tac_toe;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DiegogvTEC ll
+ */
+public class EVA2_19_TIC_TAC_TOE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tictac[][] = new int[3][3];
+        int fila, col, jug;
+        Scanner captu = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Introduce tu jugada. Fila:");
+            fila = captu.nextInt();
+            System.out.println("Introduce tu jugada. Columna:");
+            col = captu.nextInt();
+            System.out.println("Jugador (1 o 2):");
+            jug = captu.nextInt();
+            tictac[fila][col] = jug;
+        }
+        //CICLOS ANIDADOS
+        for(int i = 0; i < tictac.length; i++){
+            for(int j = 0; j < tictac[i].length; j++){
+                System.out.print("[" + tictac[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
+    
+}
